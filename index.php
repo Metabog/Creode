388df65bfe63fbe965362ee7c1662e68c4fdf40f
Bogdan Vera <bogdan.vera@fishrod.co.uk>
2015-05-07 10:10:26 +0100

    Saving to DB. Moved creode facebook class into own namespace via autoloader.

--- index.php
+++ index.php
@@ -3,29 +3,8 @@
 require_once './vendor/autoload.php';
 use Facebook\FacebookSession;
 use Facebook\FacebookRequest;
-//use Creode\Facebook\CreodeFacebookData;
+use Creode\Facebook\CreodeFacebookData;
 
-
-/*
-Encapsulates data retrieved from Facebook as a GraphObject
-*/
-class CreodeFacebookData
-{
-      /*
-      Array of data.
-      */
-      private $data;
-
-      public function setData($data)
-      {
-          $this->data = $data;
-      }
-
-      public function getData()
-      {
-           return $this->data;
-      }
-}
 
 try {
 
@@ -35,6 +14,13 @@ try {
   //get facebook stuff
   $appid = $config['fb_appid'];
   $secret = $config['fb_secret'];
+  $dbname = $config['dbname'];
+  $dbhost = $config['dbhost'];
+  $dbuser = $config['dbuser'];
+  $dbpassword = $config['dbpassword'];
+  $dbport = $config['dbport'];
+
+
   FacebookSession::setDefaultApplication($appid, $secret);
 
   //obtain a public access token
@@ -47,14 +33,94 @@ try {
   $graphObject = $response->getGraphObject();
   $dataArray = $graphObject->asArray();
 
-  //save the data in a Creode object... not sure what the point of this is
-  $data = new CreodeFacebookData();
-  $data->setData($dataArray["data"]);
+  //Connect to the postgres database
+  $dbh = new PDO("pgsql:host=$dbhost;dbname=$dbname", $dbuser, $dbpassword);
+
+  /*save the data in an array of Creode objects... not really useful here, just to fullfill tech test requirements. Also upsert the items into a postgres
+  database. Existing posts will be updates instead of being re-inserted.*/
+
+  $creodes = array();
+  foreach($dataArray["data"] as $datum)
+  {
+    $post = new CreodeFacebookData();
+
+    //populate data object with fields that exist.
+
+    if(property_exists($datum, "message"))
+    {
+    $post->setMessage($datum->message);
+    }
+    if(property_exists($datum, "from"))
+    {
+    $post->setName($datum->from->name);
+    }
+    if(property_exists($datum, "likes"))
+    {
+    $post->setNumLikes(count($datum->likes->data));
+    }
+    if(property_exists($datum, "picture"))
+    {
+    $post->setPicture($datum->picture);
+    }
+    if(property_exists($datum, "link"))
+    {
+    $post->setLink($datum->link);
+    }
+    if(property_exists($datum, "created_time"))
+    {
+    $post->setCreatedTime($datum->created_time);
+    }
+    if(property_exists($datum, "id"))
+    {
+    $post->setUuid($datum->id);
+    }
 
 
-  echo "<pre>";
-  var_dump($dataArray["data"][3]);
-  echo "</pre>";
+    array_push($creodes, $datum);
+
+
+    //do an upsert to update the db with changes or now posts
+
+    $uuid = $post->getUuid();
+    $message = $post->getMessage();
+    $name = $post->getName();
+    $numlikes = $post->getNumLikes();
+    $picture = $post->getPicture();
+    $link = $post->getLink();
+    $createdtime = $post->getCreatedTime();
+
+    $stmt = $dbh->prepare("INSERT INTO posts (uuid, message, name, numlikes, picture, link, createdtime) SELECT :uuid, :message, :name, :numlikes, :picture, :link, :createdtime WHERE NOT EXISTS (SELECT 1 FROM posts WHERE uuid=:uuid)");
+
+    $stmt->bindParam(':uuid', $uuid);
+    $stmt->bindParam(':message', $message);
+    $stmt->bindParam(':name', $name);
+    $stmt->bindParam(':numlikes', $numlikes);
+    $stmt->bindParam(':picture', $picture);
+    $stmt->bindParam(':link', $link);
+    $stmt->bindParam(':createdtime', $createdtime);
+
+    $stmt->execute();
+
+    $stmt = $dbh->prepare("UPDATE posts SET uuid=:uuid, message=:message, name=:name, numlikes=:numlikes, picture=:picture, link=:link, createdtime=:createdtime WHERE uuid=:uuid");
+    $stmt->bindParam(':uuid', $uuid);
+    $stmt->bindParam(':message', $message);
+    $stmt->bindParam(':name', $name);
+    $stmt->bindParam(':numlikes', $numlikes);
+    $stmt->bindParam(':picture', $picture);
+    $stmt->bindParam(':link', $link);
+    $stmt->bindParam(':createdtime', $createdtime);
+
+    
+
+    $stmt->execute();
+
+
+
+  }
+
+
+
+  
 
   // specify where to look for templates
   $loader = new Twig_Loader_Filesystem('templates');
--- src/Facebook/CreodeFacebookData.php
+++ src/Facebook/CreodeFacebookData.php
@@ -1,8 +1,90 @@
-<? namespace Creode\Facebook;
+<?php 
+namespace Creode\Facebook;
 
-public class CreodeFacebookData{
-	    public function __construct()
-	    {
 
-	    }
+/*
+Represents a facebook post.
+*/
+class CreodeFacebookData
+{
+
+      private $message=null;
+      private $name=null;
+      private $numLikes=null;
+      private $picture=null;
+      private $link=null;
+      private $createdTime=null;
+      private $uuid=null;
+
+      public function setMessage($message)
+      {
+          $this->message = $message;
+      }
+
+      public function getMessage()
+      {
+           return $this->message;
+      }
+
+      public function setName($name)
+      {
+          $this->name = $name;
+      }
+
+      public function getName()
+      {
+           return $this->name;
+      }
+
+      public function setNumLikes($numLikes)
+      {
+          $this->numLikes = $numLikes;
+      }
+
+      public function getNumLikes()
+      {
+           return $this->numLikes;
+      }
+
+      public function setPicture($picture)
+      {
+          $this->picture = $picture;
+      }
+
+      public function getPicture()
+      {
+           return $this->picture;
+      }
+
+      public function setLink($link)
+      {
+          $this->link = $link;
+      }
+
+      public function getLink()
+      {
+           return $this->link;
+      }
+
+      public function setCreatedTime($createdTime)
+      {
+          $this->createdTime = $createdTime;
+      }
+
+      public function getCreatedTime()
+      {
+           return $this->createdTime;
+      }
+
+      public function setUuid($uuid)
+      {
+          $this->uuid = $uuid;
+      }
+
+      public function getUuid()
+      {
+          return $this->uuid;
+      }
+
 }
+?>
